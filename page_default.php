0d471158ab3247e8bec03c1b09238084cf79a257
Andrei Miranda Fortunato <andreifortunato@pmni>
2020-12-28 16:39:29 -0300

    first commit

---
+++ page_default.php
@@ -0,0 +1,231 @@
+<!-- wp:paragraph -->
+<p>NOVIDADES NA BIBLIOTECA </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:jetpack/slideshow {"align":"full","ids":[59,62,63,64],"sizeSlug":"full"} -->
+<div class="wp-block-jetpack-slideshow alignfull" data-effect="slide"><div class="wp-block-jetpack-slideshow_container swiper-container"><ul class="wp-block-jetpack-slideshow_swiper-wrapper swiper-wrapper"><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-59" data-id="59" src="https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura4-1.jpg?w=1024"/></figure></li><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-62" data-id="62" src="https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura1-1.jpg"/></figure></li><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-63" data-id="63" src="https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura2-1.jpg"/></figure></li><li class="wp-block-jetpack-slideshow_slide swiper-slide"><figure><img alt="" class="wp-block-jetpack-slideshow_image wp-image-64" data-id="64" src="https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura3-1.jpg"/></figure></li></ul><a class="wp-block-jetpack-slideshow_button-prev swiper-button-prev swiper-button-white" role="button"></a><a class="wp-block-jetpack-slideshow_button-next swiper-button-next swiper-button-white" role="button"></a><a aria-label="Pause Slideshow" class="wp-block-jetpack-slideshow_button-pause" role="button"></a><div class="wp-block-jetpack-slideshow_pagination swiper-pagination swiper-pagination-white"></div></div></div>
+<!-- /wp:jetpack/slideshow -->
+
+<!-- wp:group {"align":"full","backgroundColor":"background"} -->
+<div class="wp-block-group alignfull has-background-background-color has-background"><div class="wp-block-group__inner-container"><!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-bottom-half","fontSize":"small"} -->
+<p class="has-text-align-center margin-bottom-half has-small-font-size"><strong>Serviços</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","className":"margin-top-half"} -->
+<h2 class="has-text-align-center margin-top-half">O que fazemos</h2>
+<!-- /wp:heading -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:gallery {"ids":[43],"linkTo":"none"} -->
+<figure class="wp-block-gallery columns-1 is-cropped"><ul class="blocks-gallery-grid"><li class="blocks-gallery-item"><figure><img src="https://arteculturaniart.files.wordpress.com/2020/11/cropped-capa-cultura1.png?w=512" alt="" data-id="43" class="wp-image-43"/></figure></li></ul></figure>
+<!-- /wp:gallery -->
+
+<!-- wp:paragraph -->
+<p>Produzimos caixas de vegetais orgânicos acessíveis, sazonais e, na medida do possível, frescos e locais.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:gallery {"ids":[46],"linkTo":"none"} -->
+<figure class="wp-block-gallery columns-1 is-cropped"><ul class="blocks-gallery-grid"><li class="blocks-gallery-item"><figure><img src="https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura2.jpg?w=275" alt="" data-id="46" class="wp-image-46"/></figure></li></ul></figure>
+<!-- /wp:gallery -->
+
+<!-- wp:paragraph -->
+<p>Temos diferentes tamanhos de caixas disponíveis para vários tipos de apetite, de individual a familiar.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:gallery {"ids":[43],"linkTo":"none"} -->
+<figure class="wp-block-gallery columns-1 is-cropped"><ul class="blocks-gallery-grid"><li class="blocks-gallery-item"><figure><img src="https://arteculturaniart.files.wordpress.com/2020/11/cropped-capa-cultura1.png?w=512" alt="" data-id="43" class="wp-image-43"/></figure></li></ul></figure>
+<!-- /wp:gallery -->
+
+<!-- wp:paragraph -->
+<p>Somos flexíveis com os itens das caixas. Você pode alterar os itens do seu pedido por alguma preferência sua.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:group -->
+
+<!-- wp:cover {"url":"https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura4.jpg","id":56,"dimRatio":30,"align":"full","className":"is-style-bottom-wave"} -->
+<div class="wp-block-cover alignfull has-background-dim-30 has-background-dim is-style-bottom-wave" style="background-image:url(https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura4.jpg)"><div class="wp-block-cover__inner-container"><!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-bottom-half","fontSize":"small"} -->
+<p class="has-text-align-center margin-bottom-half has-small-font-size"><strong>Conheça-nos melhor</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","className":"margin-top-half"} -->
+<h2 class="has-text-align-center margin-top-half">Conheça nossas coleções históricas</h2>
+<!-- /wp:heading -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","className":"margin-bottom-none"} -->
+<h2 class="has-text-align-center margin-bottom-none">000</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none"} -->
+<p class="has-text-align-center margin-top-none">famílias por mês</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","className":"margin-bottom-none"} -->
+<h2 class="has-text-align-center margin-bottom-none">00</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none"} -->
+<p class="has-text-align-center margin-top-none">especialistas</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","className":"margin-bottom-none"} -->
+<h2 class="has-text-align-center margin-bottom-none">0</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none"} -->
+<p class="has-text-align-center margin-top-none">prêmios conquistados</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","className":"margin-bottom-none"} -->
+<h2 class="has-text-align-center margin-bottom-none"><strong>100%</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none"} -->
+<p class="has-text-align-center margin-top-none">de clientes satisfeitos</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"align":"full"} -->
+<div class="wp-block-group alignfull"><div class="wp-block-group__inner-container"><!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:gallery {"ids":[60],"linkTo":"none"} -->
+<figure class="wp-block-gallery columns-1 is-cropped"><ul class="blocks-gallery-grid"><li class="blocks-gallery-item"><figure><img src="https://arteculturaniart.files.wordpress.com/2020/11/casa-de-cultura5.jpg?w=1024" alt="" data-id="60" class="wp-image-60"/></figure></li></ul></figure>
+<!-- /wp:gallery -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide","className":"margin-top-none"} -->
+<div class="wp-block-columns alignwide margin-top-none"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:jetpack/rating-star {"rating":5,"align":"center","className":"margin-bottom-half"} -->
+<figure class="wp-block-jetpack-rating-star margin-bottom-half" style="text-align:center"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></figure>
+<!-- /wp:jetpack/rating-star -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-half"} -->
+<p class="has-text-align-center margin-top-half"><strong>"Serviço excelente"</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-bottom-none","fontSize":"small"} -->
+<p class="has-text-align-center margin-bottom-none has-small-font-size">O serviço é incrível! Eu realmente espero que alguns deles estejam usando seu serviço.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none","fontSize":"small"} -->
+<p class="has-text-align-center margin-top-none has-small-font-size"><strong>Fiona</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:jetpack/rating-star {"rating":5,"align":"center","className":"margin-bottom-half"} -->
+<figure class="wp-block-jetpack-rating-star margin-bottom-half" style="text-align:center"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></figure>
+<!-- /wp:jetpack/rating-star -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-half"} -->
+<p class="has-text-align-center margin-top-half"><strong>"Amei a flexibilidade"</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-bottom-none","fontSize":"small"} -->
+<p class="has-text-align-center margin-bottom-none has-small-font-size">Estamos muito contentes com a caixa e apreciamos a opção de ter a caixa padrão ou montar a nossa.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none","fontSize":"small"} -->
+<p class="has-text-align-center margin-top-none has-small-font-size"><strong>Joseph</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:jetpack/rating-star {"rating":4,"align":"center","className":"margin-bottom-half"} -->
+<figure class="wp-block-jetpack-rating-star margin-bottom-half" style="text-align:center"><span>★</span><span>★</span><span>★</span><span>★</span></figure>
+<!-- /wp:jetpack/rating-star -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-half"} -->
+<p class="has-text-align-center margin-top-half"><strong>"Qualidade fantástica"</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-bottom-none","fontSize":"small"} -->
+<p class="has-text-align-center margin-bottom-none has-small-font-size">Aprecio muito a qualidade e a apresentação. O sabor dos vegetais é extraordinário.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none","fontSize":"small"} -->
+<p class="has-text-align-center margin-top-none has-small-font-size"><strong>Michelle</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","backgroundColor":"secondary"} -->
+<div class="wp-block-group alignfull has-secondary-background-color has-background"><div class="wp-block-group__inner-container"><!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-bottom-half","fontSize":"small"} -->
+<p class="has-text-align-center margin-bottom-half has-small-font-size"><strong>Teste com nossos vegetais</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"className":"margin-top-half"} -->
+<h3 class="has-text-align-center margin-top-half">Nossas receitas favoritas</h3>
+<!-- /wp:heading -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:latest-posts {"postsToShow":3,"displayPostContent":true,"excerptLength":40,"displayPostDate":true,"postLayout":"grid","align":"wide"} /-->
+
+<!-- wp:button {"textColor":"background","align":"center","className":"is-style-outline"} -->
+<div class="wp-block-button aligncenter is-style-outline"><a class="wp-block-button__link has-background-color has-text-color" href="/recipes">Mais receitas</a></div>
+<!-- /wp:button -->
+
+<!-- wp:spacer {"height":32} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:group -->
